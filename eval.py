bb60e6597e578bf329c1a41f9633d5d245e05f0e
Zombo <foo@bar.com>
2017-03-23 22:28:47 +0000

    Hardcoded checkpoint dir, no more flags

--- eval.py
+++ eval.py
@@ -11,33 +11,23 @@ from tensorflow.contrib import learn
 # Parameters
 # ==================================================
 
-# Eval Parameters
-tf.flags.DEFINE_string("checkpoint_dir", "", "Checkpoint directory from training run")
-
-# Misc Parameters
-tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
-
-
-FLAGS = tf.flags.FLAGS
-FLAGS._parse_flags()
-print("\nParameters:")
-for attr, value in sorted(FLAGS.__flags.items()):
-    print("{}={}".format(attr.upper(), value))
-print("")
+checkpoint_dir = "./runs/1490234541/checkpoints"
 
 # Map data into vocabulary
-vocab_path = os.path.join(FLAGS.checkpoint_dir, "..", "vocab")
+vocab_path = os.path.join(checkpoint_dir, "..", "vocab")
 vocab_processor = learn.preprocessing.VocabularyProcessor.restore(vocab_path)
 
 print("\nEvaluating...\n")
 
 # Evaluation
 # ==================================================
-checkpoint_file = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
+checkpoint_file = tf.train.latest_checkpoint(checkpoint_dir)
 graph = tf.Graph()
 with graph.as_default():
-    session_conf = tf.ConfigProto(log_device_placement=FLAGS.log_device_placement)
-    sess = tf.Session(config=session_conf)
+    # Leave this in case we want debug data
+    # session_conf = tf.ConfigProto(log_device_placement=True)
+    # sess = tf.Session(config=session_conf)
+    sess = tf.Session()
     with sess.as_default():
         handler = MessageHandler(sess, checkpoint_file, graph, vocab_processor)
         print("Awaiting messages")
